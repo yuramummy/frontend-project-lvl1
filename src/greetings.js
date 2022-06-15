caeafb31cbe79b5a60d46026df2af236db0a6fac
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-15 20:09:49 +0300

    delete greetings.js

--- src/greetings.js
+++
@@ -1,6 +0,0 @@
-import require from 'readline-sync';
-
-export const name = () => require.question('Welcome to the Brain Games!\nMay I have your name? ');
-export const userName = name();
-
-export const greetings = () => `Hello, ${userName}!`;
