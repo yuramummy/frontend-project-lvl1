51685e175f17529e03ca687cf5911d5bf17f7e4f
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-16 11:54:58 +0300

    add braingcdgame.js

---
+++ src/games/braingcdgame.js
@@ -0,0 +1,32 @@
+import require from 'readline-sync';
+import { userName, greetings, getRandom } from '../index.js';
+
+console.log(greetings());
+console.log('Find the greatest common divisor of given numbers.');
+
+const brainGCDGame = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = getRandom(0, 100);
+    const num2 = getRandom(0, 100);
+
+    const greatestCommonDivisor = (div1, div2) => {
+      if (div1 < div2) {
+        return greatestCommonDivisor(div2, div1);
+      }
+      if (div1 % div2 === 0) {
+        return div2;
+      }
+      return greatestCommonDivisor(div2, div1 % div2);
+    };
+
+    console.log(`Qestion: ${num1} ${num2}`);
+    const answer = require.question('');
+    if (answer !== greatestCommonDivisor(num1, num2).toString()) {
+      return `"${answer}" is wrong answer ;(. Correct answer was "${greatestCommonDivisor(num1, num2)}".\nLet's try again, ${userName}!`;
+    }
+    console.log('Correct!');
+  }
+  return `Congratulations, ${userName}!`;
+};
+
+export default brainGCDGame;
