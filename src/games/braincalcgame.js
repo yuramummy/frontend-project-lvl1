f8465ac491e205847ebdc7ad984e85fcd41a53cf
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-29 12:06:26 +0300

    change game logic

--- src/games/braincalcgame.js
+++ src/games/braincalcgame.js
@@ -1,38 +1,30 @@
-import require from 'readline-sync';
-import { userName, greetings } from '../index.js';
+import launcher from '../index.js';
 import getRandom from '../getrandom.js';
 
-console.log(greetings());
-console.log('What is the result of the expression?');
-const operators = ['*', '+', '-'];
-
-const brainCalcGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const operator = operators[getRandom(0, 2)];
-    const num1 = getRandom(0, 30);
-    const num2 = getRandom(0, 30);
-
-    const calculator = () => {
-      switch (operator) {
-        case '*':
-          return num1 * num2;
-        case '+':
-          return num1 + num2;
-        default:
-          return num1 - num2;
-      }
-    };
-
-    console.log(`Qestion: ${num1} ${operator} ${num2}`);
-    const answer = require.question('');
-    if (answer !== calculator().toString()) {
-      return `"${answer}" is wrong answer ;(. Correct answer was "${calculator()}".\nLet's try again, ${userName}!`;
-    }
-    console.log('Correct!');
+const calculator = (num1, num2, operator) => {
+  switch (operator) {
+    case '*':
+      return num1 * num2;
+    case '+':
+      return num1 + num2;
+    default:
+      return num1 - num2;
   }
-  return `Congratulations, ${userName}!`;
 };
 
-// console.log(brainCalcGame());
+const inputData = () => {
+  const operators = ['*', '+', '-'];
+  const operator = operators[getRandom(0, 2)];
+  const num1 = getRandom(0, 30);
+  const num2 = getRandom(0, 30);
+  const roundQuestion = `Qestion: ${num1} ${operator} ${num2}`;
+  const correctAnswer = calculator(num1, num2, operator).toString();
+  return [roundQuestion, correctAnswer];
+};
+
+const brainCalcGame = () => {
+  const gameTask = 'What is the result of the expression?';
+  launcher(gameTask, inputData);
+};
 
 export default brainCalcGame;
