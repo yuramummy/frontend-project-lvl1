dd9b0cc3c566274547ec1ba59b27b7ee9c6217e7
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-29 22:43:39 +0300

    change game logic

--- src/games/brainprimegame.js
+++ src/games/brainprimegame.js
@@ -1,38 +1,32 @@
-import require from 'readline-sync';
-import { userName, greetings } from '../index.js';
+import launcher from '../index.js';
 import getRandom from '../getrandom.js';
 
-console.log(greetings());
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  let divider = 2;
+
+  while (divider <= num / 2) {
+    if (num % divider === 0) {
+      return false;
+    }
+    divider += 1;
+  }
+  return true;
+};
+
+const inputData = () => {
+  const num = getRandom(1, 50);
+  const roundQuestion = `${num}`;
+  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  return [roundQuestion, correctAnswer];
+};
 
 const brainPrimeGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const random = getRandom(1, 50);
-
-    const isPrime = (num) => {
-      if (num < 2) {
-        return 'no';
-      }
-
-      let divider = 2;
-
-      while (divider <= num / 2) {
-        if (num % divider === 0) {
-          return 'no';
-        }
-        divider += 1;
-      }
-      return 'yes';
-    };
-
-    console.log(`Question: ${random}`);
-    const answer = require.question('');
-    if (answer !== isPrime(random)) {
-      return `"${answer}" is wrong answer ;(. Correct answer was "${isPrime(random)}".\nLet's try again, ${userName}!`;
-    }
-    console.log('Correct!');
-  }
-  return `Congratulations, ${userName}!`;
+  const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  launcher(gameTask, inputData);
 };
 
 export default brainPrimeGame;
