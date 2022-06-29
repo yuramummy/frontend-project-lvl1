05dc7449a82dd0e05b5e9682770c381ea246cb6c
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-29 12:24:20 +0300

    change game logic

--- src/games/braingcdgame.js
+++ src/games/braingcdgame.js
@@ -1,33 +1,27 @@
-import require from 'readline-sync';
-import { userName, greetings } from '../index.js';
+import launcher from '../index.js';
 import getRandom from '../getrandom.js';
 
-console.log(greetings());
-console.log('Find the greatest common divisor of given numbers.');
+const greatestCommonDivisor = (div1, div2) => {
+  if (div1 < div2) {
+    return greatestCommonDivisor(div2, div1);
+  }
+  if (div1 % div2 === 0) {
+    return div2;
+  }
+  return greatestCommonDivisor(div2, div1 % div2);
+};
+
+const inputData = () => {
+  const num1 = getRandom(0, 50);
+  const num2 = getRandom(0, 50);
+  const roundQuestion = `Qestion: ${num1} ${num2}`;
+  const correctAnswer = greatestCommonDivisor(num1, num2).toString();
+  return [roundQuestion, correctAnswer];
+};
 
 const brainGCDGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const num1 = getRandom(0, 100);
-    const num2 = getRandom(0, 100);
-
-    const greatestCommonDivisor = (div1, div2) => {
-      if (div1 < div2) {
-        return greatestCommonDivisor(div2, div1);
-      }
-      if (div1 % div2 === 0) {
-        return div2;
-      }
-      return greatestCommonDivisor(div2, div1 % div2);
-    };
-
-    console.log(`Qestion: ${num1} ${num2}`);
-    const answer = require.question('');
-    if (answer !== greatestCommonDivisor(num1, num2).toString()) {
-      return `"${answer}" is wrong answer ;(. Correct answer was "${greatestCommonDivisor(num1, num2)}".\nLet's try again, ${userName}!`;
-    }
-    console.log('Correct!');
-  }
-  return `Congratulations, ${userName}!`;
+  const gameTask = 'Find the greatest common divisor of given numbers.';
+  launcher(gameTask, inputData);
 };
 
 export default brainGCDGame;
