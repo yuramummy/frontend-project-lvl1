7a3e94c5874515fa14bf029c91c2a507c6e7ab3a
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-29 12:02:06 +0300

    add logic for all games

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import require from 'readline-sync';
 
-const name = () => require.question('Welcome to the Brain Games!\nMay I have your name? ');
-export const userName = name();
-export const greetings = () => `Hello, ${userName}!`;
+const launcher = (gameTask, inputData) => {
+  const userName = require.question('Welcome to the Brain Games!\nMay I have your name? ');
+  const roundCounter = 3;
+  console.log(`Hello, ${userName}!`);
+  console.log(gameTask);
+
+  for (let i = 0; i < roundCounter; i += 1) {
+    const [roundQuestion, correctAnswer] = inputData();
+    console.log(`Question: ${roundQuestion}`);
+    const userAnswer = require.question('');
+    if (userAnswer !== correctAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default launcher;
