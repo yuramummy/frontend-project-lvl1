9a48617884ed48a831b9de271bf54f9558f9fc5a
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-29 22:58:57 +0300

    update

--- src/games/brainprimegame.js
+++ src/games/brainprimegame.js
@@ -1,15 +1,15 @@
 import launcher from '../index.js';
 import getRandom from '../getrandom.js';
 
-const isPrime = (num) => {
-  if (num < 2) {
+const isPrime = (number) => {
+  if (number < 2) {
     return false;
   }
 
   let divider = 2;
 
-  while (divider <= num / 2) {
-    if (num % divider === 0) {
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
       return false;
     }
     divider += 1;
@@ -18,9 +18,9 @@ const isPrime = (num) => {
 };
 
 const inputData = () => {
-  const num = getRandom(1, 50);
-  const roundQuestion = `${num}`;
-  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  const number = getRandom(1, 50);
+  const roundQuestion = `${number}`;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [roundQuestion, correctAnswer];
 };
 
