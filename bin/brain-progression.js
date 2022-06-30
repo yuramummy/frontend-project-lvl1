d3858507cda6b60e54eb9a19bc8d0b97f42571e6
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-30 22:14:19 +0300

    update

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import brainProgressionGame from '../src/games/brainprogressiongame.js';
+import brainProgressionGame from '../src/games/brain-progression-game.js';
 
 brainProgressionGame();
