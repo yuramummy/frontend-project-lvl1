9325262cca65ef79c3916d3340d11a9f701f4f65
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-30 22:30:38 +0300

    rename functions and file name

---
+++ src/games/brain-prime-game.js
@@ -0,0 +1,32 @@
+import launchGame from '../index.js';
+import getRandom from '../getrandom.js';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  let divider = 2;
+
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
+      return false;
+    }
+    divider += 1;
+  }
+  return true;
+};
+
+const getInputData = () => {
+  const number = getRandom(1, 50);
+  const roundQuestion = number.toString();
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [roundQuestion, correctAnswer];
+};
+
+const brainPrimeGame = () => {
+  const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  launchGame(gameTask, getInputData);
+};
+
+export default brainPrimeGame;
