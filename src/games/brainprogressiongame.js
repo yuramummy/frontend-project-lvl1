f169817cf8ab33f01f746aedc4015a38102a5bf9
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-30 16:29:31 +0300

    change game logic

--- src/games/brainprogressiongame.js
+++ src/games/brainprogressiongame.js
@@ -1,35 +1,32 @@
 import launcher from '../index.js';
 import getRandom from '../getrandom.js';
 
+// формирование прогрессии
+// begin
 const getProgression = (startOfProgression, stepOfProgression, lengthOfProgression) => {
-  const progression = [];
+  let roundQuestion = '';
+  let hiddenNumber;
   let firstNumber = startOfProgression;
-  while (progression.length < lengthOfProgression) {
-    progression.push(firstNumber);
-    firstNumber += stepOfProgression;
-  }
-  return progression; // формирование прогрессии
-};
-
-const getQuestion = (progression, hiddenNumberIndex) => { // формирование вопроса
-  let result = '';
-  for (let i = 0; i < progression.length; i += 1) {
+  const hiddenNumberIndex = getRandom(0, lengthOfProgression - 1);
+  for (let i = 0; i < lengthOfProgression; i += 1) {
     if (i === hiddenNumberIndex) {
-      result = `${result} ..`;
-    } else result = `${result} ${progression[i]}`;
-  }
-  return result;
+      roundQuestion += ' ..';
+      hiddenNumber = firstNumber.toString();
+    } else {
+      roundQuestion = `${roundQuestion} ${firstNumber.toString()}`;
+    }
+    firstNumber += stepOfProgression;
+  } //  на выходе получаем прогрессию для вопроса и скрытое число = правильный ответ
+  return [roundQuestion, hiddenNumber];
 };
+// end
 
 const inputData = () => {
   const startOfProgression = getRandom(1, 10); // определяем начало прогрессии
   const stepOfProgression = getRandom(2, 10); // шаг прогресии
   const lengthOfProgression = getRandom(5, 10); // длина прогресии
   const progression = getProgression(startOfProgression, stepOfProgression, lengthOfProgression);
-  const hiddenNumberIndex = getRandom(0, progression.length - 1); // индекс скрытого числа
-  const hiddenNumber = progression[hiddenNumberIndex]; // скрытое число = правильный ответ
-  const roundQuestion = `${getQuestion(progression, hiddenNumberIndex)}`;
-  const correctAnswer = hiddenNumber.toString();
+  const [roundQuestion, correctAnswer] = progression;
   return [roundQuestion, correctAnswer];
 };
 
