451f1f6d72cd48ee7064b7d444350dff795bf382
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-06-30 21:54:22 +0300

    rename functions and file name

---
+++ src/games/brain-even-game.js
@@ -0,0 +1,18 @@
+import launcher from '../index.js';
+import getRandom from '../getrandom.js';
+
+const isEven = (num) => (num % 2 === 0);
+
+const getInputData = () => {
+  const num = getRandom(0, 50);
+  const roundQuestion = `${num}`;
+  const correctAnswer = isEven(num) ? 'yes' : 'no';
+  return [roundQuestion, correctAnswer];
+};
+
+const brainEvenGame = () => {
+  const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+  launcher(gameTask, getInputData);
+};
+
+export default brainEvenGame;
